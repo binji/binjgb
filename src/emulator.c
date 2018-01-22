962891bd9528f56df1f3526c2d766cc6ae851dea
Ben Smith <binjimin@gmail.com>
2018-01-21 18:47:12 -0800

    Fix blargg tests
    The blargg tests are marked as cgb-supported, which then made it load in
    CGB mode, which renders with slightly different palette colors.

--- src/emulator.c
+++ src/emulator.c
@@ -3823,7 +3823,7 @@ Result init_emulator(Emulator* e) {
   log_cart_info(e->cart_info);
   MMAP_STATE.rom_base[0] = 0;
   MMAP_STATE.rom_base[1] = 1 << ROM_BANK_SHIFT;
-  IS_CGB = e->cart_info->cgb_flag != 0;
+  IS_CGB = e->cart_info->cgb_flag == CGB_FLAG_REQUIRED;
   set_af_reg(e, 0xb0);
   REG.A = IS_CGB ? 0x11 : 0x01;
   REG.BC = 0x0013;
