37d16f105ae285d17ed748959d4010537184353d
Ben Smith <binjimin@gmail.com>
2018-03-01 20:58:14 -0800

    Use brackets for cb opcodes; mark uncommon opcodes

--- scripts/disasm.py
+++ scripts/disasm.py
@@ -78,48 +78,48 @@ OPCODE_MNEMONIC = [
 
 CB_OPCODE_MNEMONIC = [
     "rlc b",      "rlc c",   "rlc d",      "rlc e",   "rlc h",      "rlc l",
-    "rlc (hl)",   "rlc a",   "rrc b",      "rrc c",   "rrc d",      "rrc e",
-    "rrc h",      "rrc l",   "rrc (hl)",   "rrc a",   "rl b",       "rl c",
-    "rl d",       "rl e",    "rl h",       "rl l",    "rl (hl)",    "rl a",
+    "rlc [hl]",   "rlc a",   "rrc b",      "rrc c",   "rrc d",      "rrc e",
+    "rrc h",      "rrc l",   "rrc [hl]",   "rrc a",   "rl b",       "rl c",
+    "rl d",       "rl e",    "rl h",       "rl l",    "rl [hl]",    "rl a",
     "rr b",       "rr c",    "rr d",       "rr e",    "rr h",       "rr l",
-    "rr (hl)",    "rr a",    "sla b",      "sla c",   "sla d",      "sla e",
-    "sla h",      "sla l",   "sla (hl)",   "sla a",   "sra b",      "sra c",
-    "sra d",      "sra e",   "sra h",      "sra l",   "sra (hl)",   "sra a",
+    "rr [hl]",    "rr a",    "sla b",      "sla c",   "sla d",      "sla e",
+    "sla h",      "sla l",   "sla [hl]",   "sla a",   "sra b",      "sra c",
+    "sra d",      "sra e",   "sra h",      "sra l",   "sra [hl]",   "sra a",
     "swap b",     "swap c",  "swap d",     "swap e",  "swap h",     "swap l",
-    "swap (hl)",  "swap a",  "srl b",      "srl c",   "srl d",      "srl e",
-    "srl h",      "srl l",   "srl (hl)",   "srl a",   "bit 0,b",    "bit 0,c",
-    "bit 0,d",    "bit 0,e", "bit 0,h",    "bit 0,l", "bit 0,(hl)", "bit 0,a",
+    "swap [hl]",  "swap a",  "srl b",      "srl c",   "srl d",      "srl e",
+    "srl h",      "srl l",   "srl [hl]",   "srl a",   "bit 0,b",    "bit 0,c",
+    "bit 0,d",    "bit 0,e", "bit 0,h",    "bit 0,l", "bit 0,[hl]", "bit 0,a",
     "bit 1,b",    "bit 1,c", "bit 1,d",    "bit 1,e", "bit 1,h",    "bit 1,l",
-    "bit 1,(hl)", "bit 1,a", "bit 2,b",    "bit 2,c", "bit 2,d",    "bit 2,e",
-    "bit 2,h",    "bit 2,l", "bit 2,(hl)", "bit 2,a", "bit 3,b",    "bit 3,c",
-    "bit 3,d",    "bit 3,e", "bit 3,h",    "bit 3,l", "bit 3,(hl)", "bit 3,a",
+    "bit 1,[hl]", "bit 1,a", "bit 2,b",    "bit 2,c", "bit 2,d",    "bit 2,e",
+    "bit 2,h",    "bit 2,l", "bit 2,[hl]", "bit 2,a", "bit 3,b",    "bit 3,c",
+    "bit 3,d",    "bit 3,e", "bit 3,h",    "bit 3,l", "bit 3,[hl]", "bit 3,a",
     "bit 4,b",    "bit 4,c", "bit 4,d",    "bit 4,e", "bit 4,h",    "bit 4,l",
-    "bit 4,(hl)", "bit 4,a", "bit 5,b",    "bit 5,c", "bit 5,d",    "bit 5,e",
-    "bit 5,h",    "bit 5,l", "bit 5,(hl)", "bit 5,a", "bit 6,b",    "bit 6,c",
-    "bit 6,d",    "bit 6,e", "bit 6,h",    "bit 6,l", "bit 6,(hl)", "bit 6,a",
+    "bit 4,[hl]", "bit 4,a", "bit 5,b",    "bit 5,c", "bit 5,d",    "bit 5,e",
+    "bit 5,h",    "bit 5,l", "bit 5,[hl]", "bit 5,a", "bit 6,b",    "bit 6,c",
+    "bit 6,d",    "bit 6,e", "bit 6,h",    "bit 6,l", "bit 6,[hl]", "bit 6,a",
     "bit 7,b",    "bit 7,c", "bit 7,d",    "bit 7,e", "bit 7,h",    "bit 7,l",
-    "bit 7,(hl)", "bit 7,a", "res 0,b",    "res 0,c", "res 0,d",    "res 0,e",
-    "res 0,h",    "res 0,l", "res 0,(hl)", "res 0,a", "res 1,b",    "res 1,c",
-    "res 1,d",    "res 1,e", "res 1,h",    "res 1,l", "res 1,(hl)", "res 1,a",
+    "bit 7,[hl]", "bit 7,a", "res 0,b",    "res 0,c", "res 0,d",    "res 0,e",
+    "res 0,h",    "res 0,l", "res 0,[hl]", "res 0,a", "res 1,b",    "res 1,c",
+    "res 1,d",    "res 1,e", "res 1,h",    "res 1,l", "res 1,[hl]", "res 1,a",
     "res 2,b",    "res 2,c", "res 2,d",    "res 2,e", "res 2,h",    "res 2,l",
-    "res 2,(hl)", "res 2,a", "res 3,b",    "res 3,c", "res 3,d",    "res 3,e",
-    "res 3,h",    "res 3,l", "res 3,(hl)", "res 3,a", "res 4,b",    "res 4,c",
-    "res 4,d",    "res 4,e", "res 4,h",    "res 4,l", "res 4,(hl)", "res 4,a",
+    "res 2,[hl]", "res 2,a", "res 3,b",    "res 3,c", "res 3,d",    "res 3,e",
+    "res 3,h",    "res 3,l", "res 3,[hl]", "res 3,a", "res 4,b",    "res 4,c",
+    "res 4,d",    "res 4,e", "res 4,h",    "res 4,l", "res 4,[hl]", "res 4,a",
     "res 5,b",    "res 5,c", "res 5,d",    "res 5,e", "res 5,h",    "res 5,l",
-    "res 5,(hl)", "res 5,a", "res 6,b",    "res 6,c", "res 6,d",    "res 6,e",
-    "res 6,h",    "res 6,l", "res 6,(hl)", "res 6,a", "res 7,b",    "res 7,c",
-    "res 7,d",    "res 7,e", "res 7,h",    "res 7,l", "res 7,(hl)", "res 7,a",
+    "res 5,[hl]", "res 5,a", "res 6,b",    "res 6,c", "res 6,d",    "res 6,e",
+    "res 6,h",    "res 6,l", "res 6,[hl]", "res 6,a", "res 7,b",    "res 7,c",
+    "res 7,d",    "res 7,e", "res 7,h",    "res 7,l", "res 7,[hl]", "res 7,a",
     "set 0,b",    "set 0,c", "set 0,d",    "set 0,e", "set 0,h",    "set 0,l",
-    "set 0,(hl)", "set 0,a", "set 1,b",    "set 1,c", "set 1,d",    "set 1,e",
-    "set 1,h",    "set 1,l", "set 1,(hl)", "set 1,a", "set 2,b",    "set 2,c",
-    "set 2,d",    "set 2,e", "set 2,h",    "set 2,l", "set 2,(hl)", "set 2,a",
+    "set 0,[hl]", "set 0,a", "set 1,b",    "set 1,c", "set 1,d",    "set 1,e",
+    "set 1,h",    "set 1,l", "set 1,[hl]", "set 1,a", "set 2,b",    "set 2,c",
+    "set 2,d",    "set 2,e", "set 2,h",    "set 2,l", "set 2,[hl]", "set 2,a",
     "set 3,b",    "set 3,c", "set 3,d",    "set 3,e", "set 3,h",    "set 3,l",
-    "set 3,(hl)", "set 3,a", "set 4,b",    "set 4,c", "set 4,d",    "set 4,e",
-    "set 4,h",    "set 4,l", "set 4,(hl)", "set 4,a", "set 5,b",    "set 5,c",
-    "set 5,d",    "set 5,e", "set 5,h",    "set 5,l", "set 5,(hl)", "set 5,a",
+    "set 3,[hl]", "set 3,a", "set 4,b",    "set 4,c", "set 4,d",    "set 4,e",
+    "set 4,h",    "set 4,l", "set 4,[hl]", "set 4,a", "set 5,b",    "set 5,c",
+    "set 5,d",    "set 5,e", "set 5,h",    "set 5,l", "set 5,[hl]", "set 5,a",
     "set 6,b",    "set 6,c", "set 6,d",    "set 6,e", "set 6,h",    "set 6,l",
-    "set 6,(hl)", "set 6,a", "set 7,b",    "set 7,c", "set 7,d",    "set 7,e",
-    "set 7,h",    "set 7,l", "set 7,(hl)", "set 7,a",
+    "set 6,[hl]", "set 6,a", "set 7,b",    "set 7,c", "set 7,d",    "set 7,e",
+    "set 7,h",    "set 7,l", "set 7,[hl]", "set 7,a",
 ]
 
 CONTROL_OPCODES = {}
@@ -138,6 +138,23 @@ for op in [0xc2, 0xc3, 0xca, 0xd2, 0xda]: ADDR_OPCODES[op] = 'jp'
 for op in [0xc4, 0xcc, 0xcd, 0xd4, 0xdc]: ADDR_OPCODES[op] = 'call'
 
 TERMINATOR_OPCODES = set([0x18, 0xc3, 0xc9, 0xd9, 0xe9])
+
+UNCOMMON_OPCODES = set([
+  0x00,  # nop
+  0x10,  # stop
+  0x27,  # daa
+  0x40,  # ld b,b
+  0x49,  # ld c,c
+  0x52,  # ld d,d
+  0x5b,  # ld e,e
+  0x64,  # ld h,h
+  0x6d,  # ld l,l
+  0x7f,  # ld a,a
+  0xc7,  # rst $0
+  0xd9,  # reti
+  0xf7,  # rst $30
+  0xff,  # rst $38
+])
 
 KNOWN_ADDRS = {
   0x8000: 'VRAM',
@@ -440,6 +457,10 @@ class ROM(object):
         FlushPendingData()
         maybe_code = self.usage[loc] != 3
         s, opcode, oplen = self.Disassemble(loc)
+
+        if opcode in UNCOMMON_OPCODES or oplen == 0:
+          s += ' ; **TODO** '
+
         if oplen == 0: oplen += 1
         op_bytes = self.data[loc:loc+oplen]
 
