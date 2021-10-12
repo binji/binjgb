74b0a65bf38be93744ebaf401756752e7dd62bd4
Ben Smith <binjimin@gmail.com>
2021-10-12 13:30:50 -0700

    Move rom filename up to user config section

--- docs/simple.js
+++ docs/simple.js
@@ -10,6 +10,7 @@
 "use strict";
 
 // User configurable.
+const ROM_FILENAME = 'porklike.gb';
 const ENABLE_REWIND = true;
 const ENABLE_PAUSE = false;
 const ENABLE_SWITCH_PALETTES = true;
@@ -123,7 +124,7 @@ const vm = new VM();
 
 // Load a ROM.
 (async function go() {
-  let response = await fetch('porklike.gb');
+  let response = await fetch(ROM_FILENAME);
   let romBuffer = await response.arrayBuffer();
   const extRam = new Uint8Array(JSON.parse(localStorage.getItem('extram')));
   Emulator.start(await binjgbPromise, romBuffer, extRam);
