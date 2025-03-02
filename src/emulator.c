66d69e90b4c79ccc35a9a1976bca16093c44632e
SelvinPL <przemek.sulikowski@gmail.com>
2025-03-02 04:31:18 +0100

    Resolving TODO: LCDC bit 4 (#63)

--- src/emulator.c
+++ src/emulator.c
@@ -5209,13 +5209,15 @@ void emulator_render_vram(Emulator* e, u32* buffer) {
 void emulator_render_background(Emulator* e, u32* buffer, int type) {
   memset(buffer, 0, sizeof(u32) * 256 * 256);
   int bank = 0;
-  int tile_map = (type & 1) ? 0x400 : 0;
+  int tile_map = 0x1800 + ((type & 1) ? 0x400 : 0);
   for (int ty = 0; ty < 32; ty++) {
     for (int tx = 0; tx < 32; tx++) {
+      u8 tile = VRAM.data[tile_map + tx + ty * 32];
+      int offset = 0;
+      if(tile < 128)
+         offset = (LCDC.bg_tile_data_select ==  TILE_DATA_8000_8FFF) ? 0 : 0x1000;
       for (int row = 0; row < 8; row++) {
-        int tile = VRAM.data[0x1800 + tile_map + tx + ty * 32];
-        // TODO: LCDC bit 4
-        int n = tile * 16 + row * 2;
+        int n = offset + tile * 16 + row * 2;
         u8 a = VRAM.data[n];
         u8 b = VRAM.data[n + 1];
         for (int x = 0; x < 8; x++) {
